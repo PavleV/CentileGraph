a4dff2b6a71841d03be64bd930a81e90e8269f88
Pavle Vrljicak <a007678@007678.local>
2023-05-26 12:10:17 +0100

    add validation method

--- R/CentileGraph.R
+++ R/CentileGraph.R
@@ -1,5 +1,22 @@
 
-setClass("CentileGraph", slots=list(cycle_day="numeric",value="numeric"),processed="list")
+# validity method for CentileGraph class
+check_centilegraph <- function(object){
+
+  errors <- character()
+
+  if(length(object@cycle_day) != length(object@value)){
+    msg <- "Cycle days and values must be of same length."
+    errors <- c(errors,msg)
+
+  }
+
+
+  if(length(errors) == 0) TRUE else errors
+
+
+}
+
+setClass("CentileGraph", slots=list(cycle_day="numeric",value="numeric",processed="list"), validity= check_centilegraph)
 
 #' Create a CentileGraph object
 #'
@@ -14,6 +31,6 @@ setClass("CentileGraph", slots=list(cycle_day="numeric",value="numeric"),process
 
 createCentileGraph <- function(cycle_day,value){
 
-  new("CentileGraph",cycle_day=cycle_day,value=value)
+  new("CentileGraph",cycle_day=cycle_day,value=value,processed=list())
 
 }
